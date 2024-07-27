4abff30043bc8e0cbaf7a73829cf9d77a1ee4db1
Bailey Frye <baileyfrye@Baileys-Mac-mini.local>
2024-07-27 14:25:46 -0500

    Initial commit

---
+++ app/about/page.tsx
@@ -0,0 +1,20 @@
+const AboutPage = () => {
+  return (
+    <section>
+      <h1 className='flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center text-4xl font-bold leading-none tracking-wide sm:text-6xl'>
+        We love{' '}
+        <span className='bg-primary py-2 px-4 rounded-lg tracking-widest text-white'>
+          store
+        </span>
+      </h1>
+      <p className='mt-6 text-lg tracking-wide leading-8 max-w-2xl mx-auto text-muted-foreground'>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi et quam
+        architecto cum repellendus maiores reprehenderit rerum dicta dolorem,
+        nulla blanditiis placeat sunt quo ipsum! Dolore aspernatur dolores odio
+        quia praesentium. Mollitia optio dolore doloribus numquam alias dolorem
+        inventore ratione.
+      </p>
+    </section>
+  );
+};
+export default AboutPage;
